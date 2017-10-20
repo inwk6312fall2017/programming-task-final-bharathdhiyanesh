b4457371eb8a628b08e67266c3750b77334e3a6b
bharathdhiyanesh <Bharath.selvaraj@dal.ca>
2017-10-20 15:55:54 -0300

    try_method

--- tasksample1.py
+++ tasksample1.py
@@ -1,7 +1,22 @@
+import sys
 import weather from Weather
 api="https://github.com/stevob21/weather-api"
 weather=Weather()
 
+x = None
+current_max = 0
+y = None
+
+for line in sys.stdin:
+    line = line.strip()
+
+    y, temp = line.rsplit('\t', 1)
+
+    try:
+        temp = float(temp)
+    
+
 lookup=weather.lookup()
 condition=lookup()
 print(condition['text']
+
